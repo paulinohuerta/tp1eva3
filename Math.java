58722ef2534b18e309f4821e1b11c03affbf6de3
javjimmir <javierjimenezmiranda@gmail.com>
2017-05-17 09:15:02 +0200

    Add files via upload

---
+++ Math.java
@@ -0,0 +1,21 @@
+
+public class Math {
+	
+	int a;
+	int b;
+	
+	
+	public Math(int a, int b) {
+		super();
+		this.a = a;
+		this.b = b;
+	}
+	
+	
+	public int add (){
+		return a+b;
+	}
+	
+	
+
+}
