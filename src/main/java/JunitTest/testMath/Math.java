f6aa2a24c2df1d8e8fb60e4c7830ed6f673d4cf6
javjimmir <javierjimenezmiranda@gmail.com>
2017-05-18 13:48:08 +0200

    Add files via upload

---
+++ src/main/java/JunitTest/testMath/Math.java
@@ -0,0 +1,21 @@
+package JunitTest.testMath;
+public class Math {
+	
+	int a;
+	int b;
+	
+	
+	public Math(int a, int b) {
+		super();
+		this.a = a;
+		this.b = b;
+	}
+	
+	
+	public int add (){
+		return a+b;
+	}
+	
+	
+
+}
---
+++ src/test/java/JunitTest/testMath/testMath.java
@@ -0,0 +1,35 @@
+package JunitTest.testMath;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+ public class testMath {
+    Math math;
+    @Before
+    public void setUp() throws Exception {
+        math = new Math(7, 10);
+    }
+
+    @Test
+    public void testPositiveNegativeAddition() { 
+        math = new Math(7, -3);
+        Assert.assertEquals(4, math.add());
+    }
+
+    @Test
+    public void testNegativePositiveAddition() {
+        math = new Math(-7, 14);
+        Assert.assertEquals(71, math.add());
+    }
+
+
+    @Test
+    public void testNegativeAddition() {
+        math = new Math(-7, -3);
+        Assert.assertEquals(-10, math.add());
+    }
+
+    @Test
+    public void testAdd() {
+        Assert.assertEquals(17, math.add());
+    } 
+}
